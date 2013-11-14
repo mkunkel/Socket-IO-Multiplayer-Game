1e0a4676cef01fd3ff84515d38431a07125bf163
Mike Kunkel <yourmom.tv@gmail.com>
2013-11-14 15:29:55 -0600

    client side walls

--- models/game.js
+++ models/game.js
@@ -10,15 +10,16 @@ var Game = mongoose.Schema({
 
 Game.pre('save', function(next){
   if(!this.walls.length) {
-    for(var x = 0, x < 10, x++) {
-      for(var y = 0, y < 10, y++) {
+    for(var x = 0; x < 10; x++) {
+      for(var y = 0; y < 10; y++) {
         var wall = {};
         wall.x = x;
         wall.y = y;
         wall.left = 0;
         wall.right = 0;
-        wall.up = 0;
-        wall.down = 0;
+        wall.top = 0;
+        wall.bottom = 0;
+        this.walls.push(wall);
       }
     }
 
@@ -26,6 +27,6 @@ Game.pre('save', function(next){
 
   }
   next();
-}
+});
 
 mongoose.model('Game', Game);
