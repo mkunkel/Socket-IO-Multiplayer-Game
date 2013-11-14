80a9384c2e04ef6e12ea8b8fa2d7cfcab9c4b794
Mike Kunkel <yourmom.tv@gmail.com>
2013-11-14 14:39:25 -0600

    walls in model

--- models/game.js
+++ models/game.js
@@ -3,7 +3,29 @@ var mongoose = require('mongoose');
 var Game = mongoose.Schema({
   name:      String,
   players:   [{type: mongoose.Schema.Types.ObjectId, ref: 'Player'}],
+  walls:     [{}],
+  potions:   [{}],
   createdAt: {type: Date, default: Date.now}
 });
+
+Game.pre('save', function(next){
+  if(!this.walls.length) {
+    for(var x = 0, x < 10, x++) {
+      for(var y = 0, y < 10, y++) {
+        var wall = {};
+        wall.x = x;
+        wall.y = y;
+        wall.left = 0;
+        wall.right = 0;
+        wall.up = 0;
+        wall.down = 0;
+      }
+    }
+
+
+
+  }
+  next();
+}
 
 mongoose.model('Game', Game);
